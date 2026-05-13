42649cf6e633ce9b0f109d2ca14d0750767e4de1
iSergiuu <chirasergiu2@gmail.com>
2026-05-13 17:04:44 +0300

    bomba

--- backend/src/main/java/com/draxlmaier/assethub/AssetComplaintHubApplication.java
+++ backend/src/main/java/com/draxlmaier/assethub/AssetComplaintHubApplication.java
@@ -3,8 +3,11 @@ package com.draxlmaier.assethub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class AssetComplaintHubApplication {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(AssetComplaintHubApplication.class, args);
