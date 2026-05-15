5d584e09ed74371f79d0fcca38e775210b6e9376
Leonard Andrei <leo.andu2005@gmail.com>
2026-05-15 09:58:31 +0300

    AdminDashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,19 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
+import AdminDashboard from './pages/admin/AdminDashboard';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                {/* Când utilizatorul intră direct pe site, îl trimitem la Login */}
                 <Route path="/" element={<Navigate to="/login" replace />} />
 
-                {/* Rutele noastre principale */}
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                
-                {/* 2. Am adăugat ruta pentru Dashboard */}
+
                 <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/admin" element={<AdminDashboard />} />
                 
                 {/* (Opțional dar recomandat) Dacă scrie un link greșit (ex: /test), îl trimitem la login */}
                 <Route path="*" element={<Navigate to="/login" replace />} />
