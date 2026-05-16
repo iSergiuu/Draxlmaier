6e4cb13379e38d6c18daf11f183e24e63cd56af4
Iriminescu Liviu <liviuiriminescu146@gmail.com>
2026-05-16 17:52:18 +0300

    Implemented department methods

--- backend/src/main/java/com/draxlmaier/assethub/module/department/controller/DepartmentController.java
+++ backend/src/main/java/com/draxlmaier/assethub/module/department/controller/DepartmentController.java
@@ -1,4 +1,25 @@
 package com.draxlmaier.assethub.module.department.controller;
 
+import com.draxlmaier.assethub.module.department.dto.DepartmentResponseDTO;
+import com.draxlmaier.assethub.module.department.service.DepartmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/departments")
+@CrossOrigin(origins = "http://localhost:5173")
+@RequiredArgsConstructor
 public class DepartmentController {
+    private final DepartmentService departmentService;
+
+    @GetMapping
+    public ResponseEntity<List<DepartmentResponseDTO>> getAllDepartments(){
+        return ResponseEntity.ok(departmentService.getAllDepartments());
+    }
 }
--- backend/src/main/java/com/draxlmaier/assethub/module/department/service/DepartmentServiceImpl.java
+++ backend/src/main/java/com/draxlmaier/assethub/module/department/service/DepartmentServiceImpl.java
@@ -1,4 +1,31 @@
 package com.draxlmaier.assethub.module.department.service;
 
+import com.draxlmaier.assethub.module.department.dto.DepartmentResponseDTO;
+import com.draxlmaier.assethub.module.department.model.Department;
+import com.draxlmaier.assethub.module.department.repository.DepartmentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class DepartmentServiceImpl {
+    private final DepartmentRepository departmentRepository;
+
+    @Override
+    public List<DepartmentResponseDTO> getAllDepartments(){
+        return departmentRepository.findAll().stream()
+                .map(this::mapToDTO)
+                .collect(Collectors.toList());
+    }
+
+    private DepartmentResponseDTO mapToDTO(Department department){
+        return new DepartmentResponseDTO(
+                department.getId(),
+                department.getName(),
+                department.getCreatedAt()
+        );
+    }
 }
