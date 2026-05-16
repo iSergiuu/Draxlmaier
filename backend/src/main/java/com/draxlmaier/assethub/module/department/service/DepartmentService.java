4ffc74a17968ffa17ab0d9c1cde098f0bfab47b2
Iriminescu Liviu <liviuiriminescu146@gmail.com>
2026-05-16 18:05:27 +0300

    Implementing Statistics for departments

--- backend/src/main/java/com/draxlmaier/assethub/module/department/service/DepartmentService.java
+++ backend/src/main/java/com/draxlmaier/assethub/module/department/service/DepartmentService.java
@@ -5,4 +5,4 @@ import java.util.List;
 
 public interface DepartmentService {
     List<DepartmentResponseDTO> getAllDepartments();
- }
+}
--- backend/src/main/java/com/draxlmaier/assethub/module/department/service/DepartmentServiceImpl.java
+++ backend/src/main/java/com/draxlmaier/assethub/module/department/service/DepartmentServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class DepartmentServiceImpl {
+public class DepartmentServiceImpl implements DepartmentService {
     private final DepartmentRepository departmentRepository;
 
     @Override
