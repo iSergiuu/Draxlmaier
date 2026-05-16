f3c7f8fe05f7d1adfcbad8eafc1e7d0612800ab7
Iriminescu Liviu <liviuiriminescu146@gmail.com>
2026-05-16 18:14:07 +0300

    Update DepartmentService.java

--- backend/src/main/java/com/draxlmaier/assethub/module/department/service/DepartmentService.java
+++ backend/src/main/java/com/draxlmaier/assethub/module/department/service/DepartmentService.java
@@ -9,5 +9,5 @@ import java.util.UUID;
 public interface DepartmentService {
     List<DepartmentResponseDTO> getAllDepartments();
 
-    DepartmentStatsDTO getDepartmentStats(UUID departmentId)
+    DepartmentStatsDTO getDepartmentStats(UUID departmentId);
 }
