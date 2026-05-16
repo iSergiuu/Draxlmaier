ed109d9ef0d8b6ccaf7d605ee4721106f2d94fb8
Iriminescu Liviu <liviuiriminescu146@gmail.com>
2026-05-16 17:44:17 +0300

    Update DepartmentService.java

--- backend/src/main/java/com/draxlmaier/assethub/module/department/service/DepartmentService.java
+++ backend/src/main/java/com/draxlmaier/assethub/module/department/service/DepartmentService.java
@@ -1,4 +1,8 @@
 package com.draxlmaier.assethub.module.department.service;
 
+import com.draxlmaier.assethub.module.department.dto.DepartmentResponseDTO;
+import java.util.List;
+
 public interface DepartmentService {
-}
+    List<DepartmentResponseDTO> getAllDepartments();
+ }
