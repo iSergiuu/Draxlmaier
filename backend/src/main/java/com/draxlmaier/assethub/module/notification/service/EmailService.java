4e4dfc129f4c35884f22f1b84f02ec2a640631a9
Iriminescu Liviu <liviuiriminescu146@gmail.com>
2026-05-23 15:47:27 +0300

    Commit - notification

--- backend/src/main/java/com/draxlmaier/assethub/module/notification/service/EmailService.java
+++ backend/src/main/java/com/draxlmaier/assethub/module/notification/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.draxlmaier.assethub.module.notification.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,12 +12,18 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     public void sendEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("adresa.ta.de.test@gmail.com");
+
+        // Setăm expeditorul din proprietăți
+        message.setFrom(fromEmail);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
+
         mailSender.send(message);
     }
 }
